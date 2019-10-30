bac62e97b1c3c944608855281d09ed66b516cd07
hannaell <hannaell91@gmail.com>
2019-10-30 14:23:13 +0100

    Add google maps

---
+++ front-end/src/components/Map/index.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+ 
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+ 
+class Map extends Component {
+  static defaultProps = {
+    center: {
+      lat: 57.6316,
+      lng: 12.3332
+    },
+    zoom: 17
+  };
+ 
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '50vh', width: '50%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: process.env.REACT_APP_MAPS_API_KEY }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={57.6316794}
+            lng={12.3332669}
+            text='Painted Pirate Tattoo'
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
+ 
+export default Map;
--- front-end/src/view/sitemap.js
+++ front-end/src/view/sitemap.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Map from '../components/Map';
 
 const Sitemap = () => {
     return(
-        <div></div>
+        <Map />
     );
 }
 
