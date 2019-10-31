472e827abfc9714d7ca7d1ffe18e960e33adf467
hannaell <hannaell91@gmail.com>
2019-10-31 13:44:03 +0100

    add contactform

--- front-end/src/view/contact.js
+++ front-end/src/view/contact.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import Layout from '../components/Layout';
+import { H1 } from '../components/typo';
+import ContactForm from '../components/ContactForm';
+import Map from '../components/Map';
 
 const Contact = () => {
     return (
-        <div></div>
+        <Layout>
+            <H1 text='For questions or time booking, please fill in the form and upload a picture for inspiration.' />
+            <ContactForm />
+            <Map />
+        </Layout>
     );
 }
 
