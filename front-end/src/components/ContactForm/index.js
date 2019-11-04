92ccebef406fe0cb077fd47b26fc2ec99e9ac9b5
hannaell <hannaell91@gmail.com>
2019-11-04 10:29:40 +0100

    fix styling

--- front-end/src/components/ContactForm/index.js
+++ front-end/src/components/ContactForm/index.js
@@ -5,7 +5,8 @@ import { P, H2, H3 } from '../typo';
 const FormStyled = styled.form`
     height: 78vh;
     width: 74vw;
-    padding: 13vw;
+    padding-left: 13vw;
+    padding-top: 6.5vh;
 `;
 
 const InputStyled = styled.div`
@@ -22,9 +23,6 @@ const TextInputStyled = styled.input`
     font-size: 16px;
     background-color: #F7F7F7;
     border: 2px solid #1A1A1A;
-    /* ::placeholder {
-        font-size: 16px;
-    } */
 `;
 
 const TextAreaStyled = styled.textarea`
@@ -33,94 +31,67 @@ const TextAreaStyled = styled.textarea`
     font-size: 16px;
     background-color: #F7F7F7;
     border: 2px solid #1A1A1A;
-    /* ::placeholder {
-        font-size: 16px;
-    } */
 `;
-
-// const LabelFileStyled = styled.label`
-//     /* height: 5.5vh;
-//     width: 37vw; */
-//    :after{
-//          background-color: #1A1A1A;
-//     }
-//     :before{
-
-//     }
-// `;
-
-// const InputFileStyled = styled.input`
-//     opacity: 0;
-//     height: 5.5vh;
-//     :after{
-//          background-color: #1A1A1A;
-//     }
-//     :before{
-//         background-color: #1A1A1A;
-//     }
-// `;
 
 const ButtonStyled = styled.button`
     height: 5.5vh;
     width: 37vw;
+    margin-top: 4.4vh;
     background-color: #1A1A1A;
 `;
 
-
 class ContactForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.submitForm = this.submitForm.bind(this);
-    this.state = {
-      status: ''
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.submitForm = this.submitForm.bind(this);
+        this.state = {
+        status: ''
+        };
+    }
 
-  render() {
-    const { status } = this.state;
-    return (
-      < FormStyled
-        onSubmit={this.submitForm}
-        action = 'https://formspree.io/xknwkzeb'
-        method='POST'
-        enctype='multipart/form-data'
-      >
-        <InputStyled>
-            <label><H2 text='Name' fontWeight='bold' paddingBottom='4px' paddingTop='18px'/></label>
-            <TextInputStyled type='text' name='name' placeholder='Camille Jadermark' />
-            <label><H2 text='E-mail' fontWeight='bold' paddingBottom='4px' paddingTop='18px' /></label>
-            <TextInputStyled type='email' name='email' placeholder='camille-justine@live.se' />
-            <label><H2 text='Message' fontWeight='bold' paddingBottom='4px' paddingTop='18px' /></label>
-            <TextAreaStyled name='message' placeholder='Hello, ny name is Camille.'></TextAreaStyled>
-        </InputStyled>
-        
-            <input type='file' name='attachment' accept='image/png, image/jpeg' />
-        
-        {status === 'SUCCESS' ? <P text='Thanks!' /> : <ButtonStyled><H3 text='SEND' color='#F7F7F7'/></ButtonStyled>}
-        {status === 'ERROR' && <P text='Ooops! There was an error.' />}
-      </FormStyled>
-    );
-  }
+    render() {
+        const { status } = this.state;
+        return (
+            < FormStyled
+                onSubmit={this.submitForm}
+                action = 'https://formspree.io/xknwkzeb'
+                method='POST'
+                enctype='multipart/form-data'
+            >
+                <InputStyled>
+                    <label><H2 text='Name' fontWeight='bold' paddingBottom='4px' paddingTop='18px'/></label>
+                    <TextInputStyled type='text' name='name' placeholder='Camille Jadermark' />
+                    <label><H2 text='E-mail' fontWeight='bold' paddingBottom='4px' paddingTop='18px' /></label>
+                    <TextInputStyled type='email' name='email' placeholder='camille-justine@live.se' />
+                    <label><H2 text='Message' fontWeight='bold' paddingBottom='4px' paddingTop='18px' /></label>
+                    <TextAreaStyled name='message' placeholder='Hello, ny name is Camille.'></TextAreaStyled>
+                </InputStyled>
+                <input type='file' name='attachment' accept='image/png, image/jpeg' />
+                {status === 'SUCCESS' ? <P text='Thanks!' /> : <ButtonStyled><H3 text='SEND' color='#F7F7F7'/></ButtonStyled>}
+                {status === 'ERROR' && <P text='Ooops! There was an error.' />}
+            </FormStyled>
+        );
+    }
 
-  submitForm(ev) {
-    ev.preventDefault();
-    const form = ev.target;
-    const data = new FormData(form);
-    const xhr = new XMLHttpRequest();
-    xhr.open(form.method, form.action);
-    xhr.setRequestHeader('Accept', 'application/json');
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState !== XMLHttpRequest.DONE) return;
-      if (xhr.status === 200) {
-        form.reset();
-        this.setState({ status: 'SUCCESS' });
-      } else {
-        this.setState({ status: 'ERROR' });
-      }
-    };
-    xhr.send(data);
-    console.log(data);
-  }
+    submitForm(ev) {
+        ev.preventDefault();
+        const form = ev.target;
+        const data = new FormData(form);
+        const xhr = new XMLHttpRequest();
+        xhr.open(form.method, form.action);
+        xhr.setRequestHeader('Accept', 'application/json');
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState !== XMLHttpRequest.DONE) return;
+            if (xhr.status === 200) {
+                form.reset();
+                this.setState({ status: 'SUCCESS' });
+            } else {
+                this.setState({ status: 'ERROR' });
+            }
+        };
+        xhr.send(data);
+        console.log(data);
+    }
 }
 
 export default ContactForm;
--- front-end/src/components/Map/index.js
+++ front-end/src/components/Map/index.js
@@ -1,42 +1,57 @@
 import React, { Component } from 'react';
+import { H2 } from '../typo';
 import GoogleMapReact from 'google-map-react';
 import styled from 'styled-components';
+
+const DivStyled = styled.div`
+    height: 83vh;
+    width: 100vw;
+    margin-top: 18vh;
+    margin-bottom: 12vh;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+`;
 
 const MapStyled = styled.div`
     height: 78vh;
     width: 74vw;
     border: 2px solid #E5E5E5;
-    margin: 13vw;
+    margin-left: 13vw;
 `;
  
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
  
 class Map extends Component {
-  static defaultProps = {
-    center: {
-        lat: 57.6316,
-        lng: 12.3332
-    },
-    zoom: 17
-  };
- 
-  render() {
-    return (
-        <MapStyled>
-            <GoogleMapReact
-                bootstrapURLKeys={{ key: process.env.REACT_APP_MAPS_API_KEY }}
-                defaultCenter={this.props.center}
-                defaultZoom={this.props.zoom}
-            >
-                <AnyReactComponent
-                    lat={57.6316794}
-                    lng={12.3332669}
-                    text='Painted Pirate Tattoo'
-                />
-            </GoogleMapReact>
-        </MapStyled>
-    );
-  }
-}
+    static defaultProps = {
+        center: {
+            lat: 57.6316,
+            lng: 12.3332
+        },
+        zoom: 17
+    };
+    
+    render() {
+        return (
+            <DivStyled>
+                <H2 text='Location' fontWeight='bold' marginLeft='13vw' marginBottom='1vh' />
+                <MapStyled>
+                    <GoogleMapReact
+                        bootstrapURLKeys={{ key: process.env.REACT_APP_MAPS_API_KEY }}
+                        defaultCenter={this.props.center}
+                        defaultZoom={this.props.zoom}
+                    >
+                        <AnyReactComponent
+                            lat={57.6316794}
+                            lng={12.3332669}
+                            text='Painted Pirate Tattoo'
+                        />
+                    </GoogleMapReact>
+                </MapStyled>
+            </DivStyled>
+        );
+    }
+    }
  
 export default Map;
